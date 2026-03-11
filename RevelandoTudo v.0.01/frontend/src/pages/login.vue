82ebf97fd7baa2cdc2565c2a1fd430204b2aef9c
Diego Vicente <diegofrra083@gmail.com>
2026-03-10 23:27:00 -0300

    Add files via upload

---
+++ RevelandoTudo v.0.01/frontend/src/pages/login.vue
@@ -0,0 +1,46 @@
+<template>
+
+<div class="login-container">
+
+<h2>Login</h2>
+
+<input v-model="email" placeholder="Email" />
+<input v-model="senha" type="password" placeholder="Senha" />
+
+<button @click="login">Entrar</button>
+
+</div>
+
+</template>
+
+<script>
+
+import api from "../services/api"
+
+export default {
+
+data(){
+return{
+email:"",
+senha:""
+}
+},
+
+methods:{
+
+async login(){
+
+const response = await api.post("/auth/login",{
+email:this.email,
+senha:this.senha
+})
+
+console.log(response.data)
+
+}
+
+}
+
+}
+
+</script>
